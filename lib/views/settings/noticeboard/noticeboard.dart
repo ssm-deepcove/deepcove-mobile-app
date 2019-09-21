7a9442ddea24781cf43bebbac0ff621d51e6f442
MitchellQ <mitchell.quarrie@outlook.com>
2019-09-21 21:55:41 +1200

    Add AppBar

--- lib/views/settings/noticeboard/noticeboard.dart
+++ lib/views/settings/noticeboard/noticeboard.dart
@@ -1,4 +1,7 @@
+import 'package:discover_deep_cove/util/screen.dart';
+import 'package:discover_deep_cove/widgets/misc/text/sub_heading.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Noticeboard extends StatefulWidget {
   @override
@@ -8,8 +11,32 @@ class Noticeboard extends StatefulWidget {
 class _NoticeboardState extends State<Noticeboard> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        leading: Container(),
+        title: SubHeading(
+          'Deep Cove Noticeboard',
+          size:
+              Screen.isTablet(context) ? 30 : Screen.isSmall(context) ? 16 : 20,
+        ),
+        centerTitle: true,
+        actions: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(
+              right: Screen.width(context, percentage: 1.25),
+            ),
+            child: IconButton(
+              icon: Icon(
+                FontAwesomeIcons.sync,
+                color: Colors.white,
+              ),
+              onPressed: () {},
+            ),
+          ),
+        ],
+        backgroundColor: Theme.of(context).backgroundColor,
+        brightness: Brightness.dark,
+      ),
     );
   }
 }
