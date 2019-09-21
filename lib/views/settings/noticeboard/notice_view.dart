7e3c7222b0a9378a428085ed4b9aaf94060fdecf
MitchellQ <mitchell.quarrie@outlook.com>
2019-09-21 21:36:02 +1200

    added bolier plate views

---
+++ lib/views/settings/noticeboard/notice_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class NoticeView extends StatelessWidget {
+  final int noticeId;
+
+  NoticeView({@required this.noticeId}) : assert(noticeId != null);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
---
+++ lib/views/settings/noticeboard/noticeboard.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Noticeboard extends StatefulWidget {
+  @override
+  _NoticeboardState createState() => _NoticeboardState();
+}
+
+class _NoticeboardState extends State<Noticeboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
