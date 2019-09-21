92f6e258abd5422a0080c23493acf7bee7a46af7
MitchellQ <mitchell.quarrie@outlook.com>
2019-09-21 21:18:00 +1200

    Update sample_notices.dart

--- lib/data/sample_notices.dart
+++ lib/data/sample_notices.dart
@@ -35,9 +35,6 @@ List<Notice> notices = [
     title: "This is not an urgent test notice",
     shortDesc:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    longDesc:
-        "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type"
-        " and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic"
-        "typesetting, remaining essentially unchanged.",
+    longDesc: null,
   ),
 ];
