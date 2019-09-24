374cb4ba2e5b939c59136458becc7f4a48997b48
MitchellQ <mitchell.quarrie@outlook.com>
2019-09-24 18:57:01 +1200

    starting transitioning to responsive view

--- lib/views/settings/noticeboard/notice_view.dart
+++ lib/views/settings/noticeboard/notice_view.dart
@@ -12,106 +12,143 @@ import 'package:flutter/material.dart';
 
 class NoticeView extends StatelessWidget {
   final Notice notice;
+
   NoticeView({this.notice});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: <Widget>[
-            SizedBox(
-              height: 50,
-            ),
-            Heading(
-              notice.title,
-              size: Screen.isTablet(context)
-                  ? 30
-                  : Screen.isSmall(context) ? 16 : null,
-            ),
-            SizedBox(
-              height: 5,
-            ),
-            Text(
-              DateUtil.formatDate(notice.activatedAt),
-              style: TextStyle(
-                color: HexColor("FF999999"),
-                fontSize: Screen.isTablet(context)
-                    ? 23
-                    : Screen.isSmall(context) ? 12 : 14,
-              ),
-            ),
-            Divider(color: HexColor("FF777777"), height: 20),
-            notice.image.path == null
-                ? Container()
-                : Padding(
-                    padding: EdgeInsets.symmetric(vertical: 10),
-                    child: Container(
-                      height: Screen.width(context,
-                          percentage: Screen.isTablet(context) &&
-                                  Screen.isLandscape(context)
-                              ? 35
-                              : Screen.isTablet(context)
-                                  ? 70
-                                  : Screen.isSmall(context) ? 55 :80),
-                      width: Screen.width(context,
-                          percentage: Screen.isTablet(context) &&
-                                  Screen.isLandscape(context)
-                              ? 35
-                              : Screen.isTablet(context)
-                                  ? 70
-                                  : Screen.isSmall(context) ? 55 : 80),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        image: DecorationImage(
-                          image: FileImage(
-                            File(
-                              Env.getResourcePath(
-                                notice.image.path,
-                              ),
-                            ),
-                          ),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                  ),
-            notice.image.path == null
-                ? Container()
-                : Divider(color: HexColor("FF777777"), height: 20),
-            Flexible(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                child: ListView(
-                  children: <Widget>[
-                    BodyText(
-                      notice.shortDesc,
-                      size: Screen.isTablet(context)
-                          ? 30
-                          : Screen.isSmall(context) ? 16 : null,
-                      align: TextAlign.left,
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    BodyText(
-                      notice.longDesc,
-                      size: Screen.isTablet(context)
-                          ? 30
-                          : Screen.isSmall(context) ? 16 : null,
-                      align: TextAlign.left,
-                    ),
-                    Divider(color: HexColor("FF777777"), height: 40),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
+      body: buildContent(context),
       backgroundColor: Theme.of(context).backgroundColor,
       bottomNavigationBar: BottomBackButton(),
+    );
+  }
+
+  buildContent(BuildContext context) {
+    return (Screen.isLandscape(context))
+        ? GridView.count(
+            crossAxisCount: 2,
+            children: [
+              ListView(
+                children: [],
+              ),
+            ],
+          )
+        : Column(
+            children: <Widget>[
+              getTop(context),
+              Flexible(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListView(
+                    children: [
+                      getBottom(context),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          );
+  }
+
+  getTop(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 50,
+          ),
+          Heading(
+            notice.title,
+            size: Screen.isTablet(context)
+                ? 30
+                : Screen.isSmall(context) ? 16 : null,
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text(
+            DateUtil.formatDate(notice.activatedAt),
+            style: TextStyle(
+              color: HexColor("FF999999"),
+              fontSize: Screen.isTablet(context)
+                  ? 23
+                  : Screen.isSmall(context) ? 12 : 14,
+            ),
+          ),
+          Divider(color: HexColor("FF777777"), height: 20),
+        ],
+      ),
+    );
+  }
+
+  getBottom(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        notice.image.path == null
+            ? Container()
+            : Padding(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                child: Container(
+                  height: Screen.width(context,
+                      percentage: Screen.isTablet(context) &&
+                              Screen.isLandscape(context)
+                          ? 35
+                          : Screen.isTablet(context)
+                              ? 70
+                              : Screen.isSmall(context) ? 55 : 80),
+                  width: Screen.width(context,
+                      percentage: Screen.isTablet(context) &&
+                              Screen.isLandscape(context)
+                          ? 35
+                          : Screen.isTablet(context)
+                              ? 70
+                              : Screen.isSmall(context) ? 55 : 80),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    image: DecorationImage(
+                      image: FileImage(
+                        File(
+                          Env.getResourcePath(
+                            notice.image.path,
+                          ),
+                        ),
+                      ),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+        notice.image.path == null
+            ? Container()
+            : Divider(color: HexColor("FF777777"), height: 20),
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: <Widget>[
+              BodyText(
+                notice.shortDesc,
+                size: Screen.isTablet(context)
+                    ? 30
+                    : Screen.isSmall(context) ? 16 : null,
+                align: TextAlign.left,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              BodyText(
+                notice.longDesc,
+                size: Screen.isTablet(context)
+                    ? 30
+                    : Screen.isSmall(context) ? 16 : null,
+                align: TextAlign.left,
+              ),
+            ],
+          ),
+        ),
+        Divider(color: HexColor("FF777777"), height: 40),
+      ],
     );
   }
 }
